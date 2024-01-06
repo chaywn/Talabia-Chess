6c762e2008582e4ff71f1b8a7db9621492d9775b
ChayWN <123713452+ChayWN@users.noreply.github.com>
2024-01-06 10:52:14 +0800

    Revert "Update Point.java"
    This reverts commit 4b8ac48bc8e1142cee3a51109ba5ff4fad3ba144.

--- ChessPiece/Hourglass.java
+++ ChessPiece/Hourglass.java
@@ -1,24 +1,32 @@
 // Coding Member: 
 
+<<<<<<< HEAD
 package ChessPiece; // Assuming the 'Chess' package
+=======
+package ChessPiece;
+>>>>>>> parent of d40ab93 (Update Hourglass.java)
 
-import java.awt.Image;
+import java.awt.*;
 
 import Board.Board;
+<<<<<<< HEAD
 
 public class Hourglass extends Piece {
+=======
+>>>>>>> parent of d40ab93 (Update Hourglass.java)
 
-    public Hourglass(int x, int y, boolean isWhite, Image image) {
-        super(x, y, isWhite, image);
+public class Hourglass extends Piece{
+    
+    public Hourglass(int x, int y, Color color, Boolean flipped) {
+        super(x, y, color, flipped);
     }
 
     @Override
-    public boolean canMove(Board board, int targetX, int targetY) {
-        int xDistance = Math.abs(targetX - this.getX());
-        int yDistance = Math.abs(targetY - this.getY());
-
-        // Check for 3x2 L-shape move
-        return (xDistance == 2 && yDistance == 3) || (xDistance == 3 && yDistance == 2);
+    public boolean isMovableTo(Board board, Piece p, int x, int y) {
+        // testing purpose
+        if ((board.getPieceAt(x, y) != null) && (p.getColor() == board.getPieceAt(x, y).getColor())) {
+            return false;
+        }
+        return true;
     }
-
-
+}
