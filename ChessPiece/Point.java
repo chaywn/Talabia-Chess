1a6343de4d5d262231dce71737b4f35dbc374d57
wjua1 <123620874+wjua1@users.noreply.github.com>
2024-01-06 10:07:01 +0800

    Update Point.java

--- ChessPiece/Point.java
+++ ChessPiece/Point.java
@@ -9,16 +9,22 @@ import Board.Board;
 
 public class Point extends Piece {
     private boolean isMovingForward;
-    
-    public Point(int x, int y, boolean isWhite, Image image) {
-        super(x, y, isWhite, image);
-        this.isMovingForward = true; // Initially set to true
+
+    public Point(int x, int y, boolean isWhite) {
+        super(x, y, isWhite);
+        this.isMovingForward = true;
     }
 
     @Override
     public boolean canMove(Board board, int targetX, int targetY) {
-        // ... implementation of canMove ...
+        // Implement specific move logic for Point
     }
 
-    // ... other methods ...
+    @Override
+    public void move(int x, int y) {
+        super.move(x, y); // Call the move method of the superclass
+
+        // Additional logic specific to Point, if any
+        // For example, changing direction at the end of the board
+    }
 }
