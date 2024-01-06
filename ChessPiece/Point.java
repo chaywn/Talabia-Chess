2c0a6f0db4c1727a70dfb89a192f65f28b6cdc80
wjua1 <123620874+wjua1@users.noreply.github.com>
2024-01-06 09:47:52 +0800

    Update Point.java

--- ChessPiece/Point.java
+++ ChessPiece/Point.java
@@ -1,31 +1,24 @@
 // Coding Member: Goh Shi Yi, Choo Yun Yi
 
-package ChessPiece;
+
+package Chess;
 
 import java.awt.*;
 
 import Board.Board;
 
 public class Point extends Piece {
-
-    public Point(int x, int y, Color color, Boolean flipped) {
-        super(x, y, color, flipped);
+    private boolean isMovingForward;
+    
+    public Point(int x, int y, boolean isWhite, Image image) {
+        super(x, y, isWhite, image);
+        this.isMovingForward = true; // Initially set to true
     }
 
     @Override
-    public boolean isMovableTo(Board board, Piece p, int x, int y) {
-        // still not done (If it reaches the end of the board, it turns around and starts heading back the other way.)
-        if (p.getX() == x && p.getY() > y && (Math.abs(p.getY() - y) == 1 || Math.abs(p.getY() - y) == 2)) {
-            for (int i = Math.min(p.getY(), y) + 1; i < Math.max(p.getY(), y); i++) {
-                if (board.getPieceAt(x, i) != null) {
-                    return false;
-                }
-            }
-            if ((board.getPieceAt(x, y) != null) && (p.getColor() == board.getPieceAt(x, y).getColor())) {
-                return false;
-            }
-            return true;
-        }
-        return false;
+    public boolean canMove(Board board, int targetX, int targetY) {
+        // ... implementation of canMove ...
     }
+
+    // ... other methods ...
 }
